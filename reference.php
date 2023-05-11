341521cde749442cac884eeb85888f39244b0f1a
Szacky <63283106+Szacky@users.noreply.github.com>
2023-05-11 12:30:14 +0100

    Add files via upload

---
+++ reference.php
@@ -0,0 +1,20 @@
+<p>Reference</p>
+          <p>The following references are to web styles</p>
+          <p>1. https://www.coca-cola.co.uk/</p>
+          <p>2. https://www.coca-colacompany.com/cn/home</p>
+          <p>3. https://benskitchen.com/x3dom.php</p>
+          <p>4. https://users.sussex.ac.uk/~martinwh/3dapp/lab9/CocaCola_VM/index.php/#</p>
+          <p>The following references are to CSS styles</p>
+          <p>1. bootstrap-4.4.1.css</p>
+          <p>2. X3DOM.css http://www.x3dom.org</p>
+          <p>3. bootstrap-4.4.1.css https://getbootstrap.com</p>
+          <p>4. https://fonts.googleapis.com/css?family=Open+Sans</p>
+          <p>5. https://fonts.googleapis.com/css?family=Oswald</p>
+          <p>6. jquery.fancybox.min.css https://fancyapps.com/fancybox/</p>
+          <p>The following references are for JS</p>
+          <p>1. all.js https://fontawesome.com</p>
+          <p>2. popper.min.js https://unpkg.com/@popperjs/core@2</p>
+          <p>3. bootstrap.min.js https://getbootstrap.com</p>
+          <p>4. fontawesome-all.min.js http://fontawesome.com</p>
+          <p>5. x3dom-1.7.2/x3dom.js http://www.x3dom.org</p>
+          <p>6. jquery.fancybox.min.js http://fancyapps.com/fancybox/</p>
